536aaa82d1acd99c7668ab1e94fd29e8d0b7229a
Shijo Varghese <shijo.varghese@velrada.com>
2018-04-10 01:36:59 +0800

    commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=qnaaz-Web-Deploy.pubxml -p:Password=qWrPYctKLvhGs8zv1DiunPXq3xltSWAk885JklsjkoWfpu8EqCpAFe8DLJ1n
+
